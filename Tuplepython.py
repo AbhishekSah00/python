42bfcc0cc3a335f1bc7bf4327c9f97bd123a8aa7
AbhishekSah00 <satyamshah082@gmail.com>
2023-04-20 12:22:24 +0545

    Tuple file

---
+++ Tuplepython.py
@@ -0,0 +1,18 @@
+Python Tuple
+mytuple = ("apple", "mango", "orange")
+print(mytuple)
+
+multiple items in a single variable.
+Tuple is one of the built-in data types.
+ordered, unchangeable, and Allow duplicate 
+
+single item has, a comma
+eg;-
+a tuple can store different data types:
+Using the tuple method to use the tuple:
+thistuple=tuple(("apple",  "mango", "cherry"))
+print(thistuple)
+
+Tuple is a collection that is ordered and unchangeable. allow duplicate.
+ 
+
